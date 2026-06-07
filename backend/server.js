3bd0069d2a5e2d1b02b28c32cd0f0b374cb4a233
Yuvraj Singh <thinkyuvraj@gmail.com>
2026-06-07 14:40:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,28 +4,6 @@ const app = require("./src/app");
 const pool = require("./src/config/db");
 
 const PORT = process.env.PORT || 5000;
-const jobRoutes =
-require("./src/routes/jobRoutes");
-app.use("/api", jobRoutes);
-const resumeRoutes =
-require("./src/routes/resumeRoutes");
-
-app.use("/api", resumeRoutes);
-
-const screeningRoutes = require("./src/routes/screeningRoutes");
-app.use("/api", screeningRoutes);
-
-const rankingRoutes = require("./src/routes/rankingRoutes");
-app.use("/api", rankingRoutes);
-
-const bulkScreeningRoutes = require("./src/routes/bulkScreeningRoutes");
-app.use("/api", bulkScreeningRoutes);
-
-const interviewRoutes = require("./src/routes/interviewRoutes");
-app.use("/api", interviewRoutes);
-
-const assistantRoutes = require("./src/routes/assistantRoutes");
-app.use("/api", assistantRoutes);
 
 pool.connect()
   .then(() => {
